22e2d42f7bcf5f89f1275bcfaf15510fa4ec3b37
Dhanabal <dhanabal@vinu-Vostro-3560.(none)>
2014-01-02 13:04:48 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kavadi::Application.config.secret_key_base = '0642faff7c3bf6e9d506634b4f5b001ee14fc2bbaf543fd186c3871fb71a616c1a1992f49149d11e483261cb7452887e1062d2a595ab3a6beb8314ffd0ee766a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kavadi::Application.config.session_store :cookie_store, key: '_kavadi_session'
